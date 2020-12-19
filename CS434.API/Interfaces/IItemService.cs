40dd8e52a131b19561b2a84d50340e249b02c3a3
ozanozbirecikli <41952823+ozanozbirecikli@users.noreply.github.com>
2020-12-19 21:33:18 +0300

    Items CRUD operations added.

---
+++ CS434.API/Interfaces/IItemService.cs
@@ -0,0 +1,17 @@
+﻿using LibraryApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryApplication.Business.Abstract
+{
+	public interface IItemService
+	{
+		List<Item> GetAllItems();
+		Item GetItemById(int id);
+		Item CreateItem(Item item);
+		Item UpdateItem(Item item);
+		void DeleteItem(int id);
+
+	}
+}
